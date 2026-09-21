6810132ec1638b2408e91544dc5314f907b3f14c
yashsdhinoja <yash.s.dhinoja7007@gmail.com>
2026-09-21 20:04:45 +0530

    Task Added in react

---
+++ Execrise/accident/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return(
+    <div className='main'>
+        <i className='fa-solid fa-jet-fighter plane'></i>
+        <i className='fa-solid fa-bomb bomb'></i>
+    </div>
+  )
+}
+
+export default App;
---
+++ mounting/src/LifeCycle.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+class Lifecycle extends React.Component
+{
+    constructor(props)
+    {
+        super(props);
+        this.state={count :0};
+        console.log('1. Constructor');
+    }
+
+    static getDrivedStateFromProps(props, state)
+    {
+        console.log('2. getDrivedStateFromProps');
+        return null;
+    }
+
+    render()
+    {
+        console.log('3. render');
+        return(
+            <h1>Count : {this.state.count}</h1>
+        );
+    }
+
+    componentDidMount()
+    {
+        console.log('4. ComponentDidMount');
+        setTimeout(()=> {
+            this.setState({count :1});
+        },2000);
+        return null;
+    }
+
+
+    shouldComponentUpdate()
+    {
+        console.log('5. shouldComponentUpdate');
+        return true;
+    }
+
+    getSnapshotBeforeUpdate(prevProps, prevState)
+    {
+        console.log('6. getSnapshotBeforeUpdate');
+        return null;
+    }
+
+    componentDidUpdate()
+    {
+        console.log('7. coponentDidUpdate');
+        return null;
+    }
+
+    componentnWillUnmount() {
+        console.log('8. componentnWillUnmount');
+        return null;
+    }
+
+}
+export default Lifecycle
